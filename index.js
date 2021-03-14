fffe469f81838e659fc6145322f2a64ff124b8e6
Jamie H Pham <jpham1109@gmail.com>
2021-03-14 14:06:12 -0400

    Done

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(string) {
+  const time = string.split(':');
+  const hour = parseInt(time[0]);
+  if (hour < 12) {
+    return "Good Morning";
+  } else if ( hour > 12 && hour < 17) {
+    return "Good Afternoon";
+  } else if ( hour > 17 && hour <= 23) {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
